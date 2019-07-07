09487695319ece2db0169883b18a07add0e4166f
yeroc8 <coreyshive@MacBook-Pro.fios-router.home>
2019-07-07 19:10:52 -0400

    Add defaults to Character
    Useful for basic monsters

--- char/character.py
+++ char/character.py
@@ -1,7 +1,13 @@
+from .stats import random_monster
+
+
 class Character:
-    def __init__(self, gender, name, stats, classType='Monster', level=1):
+    def __init__(self, gender=None, name="Slime", classType="Monster", level=1, stats=None):
         self.gender = gender
         self.name = name
-        self.level = level
-        self.stats = stats
         self.classType = classType
+        self.level = level
+        if stats:
+            self.stats = stats
+        else:
+            self.stats = random_monster(level)
